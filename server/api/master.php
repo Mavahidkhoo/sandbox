390c643aaabd033e0bf64ad61cf22c4c98de5a2b
androsein <androsein@yahoo.com>
2020-02-15 23:31:45 +0330

    Added formatDate function

--- server/api/master.php
+++ server/api/master.php
@@ -9,3 +9,8 @@ if (!isset($_POST['CRUD'])) {
     ));
 }
 $operation = new Operations($databseConfig['server'], $databseConfig['database'], $databseConfig['username'], $databseConfig['password']);
+
+function formatDate($date)
+{
+    return substr($date, 0, 4) . '/' . substr($date, 4, 2) . '/' . substr($date, 6, 2);
+}
