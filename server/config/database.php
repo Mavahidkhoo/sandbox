2adb184316bfc8076d8684b00aa9610c4b632c5e
androsein <androsein@yahoo.com>
2020-02-15 10:28:43 +0330

    Added config file for database

---
+++ server/config/database.php
@@ -0,0 +1,8 @@
+<?php
+// Change these values with database login values in your localhost or host
+$databseConfig = array(
+    'server' => 'localhost',
+    'database' => 'db_tmp',
+    'username' => 'root',
+    'password' => ''
+);
