2a317c2ef10e066df63d343588e9958b08a305a6
androsein <androsein@yahoo.com>
2020-02-15 13:48:40 +0330

    Add check CRUD post parameter in master for code performance

--- server/api/master.php
+++ server/api/master.php
@@ -3,5 +3,9 @@ session_start();
 require_once '../config/database.php';
 require_once '../class/operations.class.php';
 require_once '../class/utils.class.php';
-
+if (!isset($_POST['CRUD'])) {
+    Utils::result(true, array(
+        'message' => 'Missing post parameters'
+    ));
+}
 $operation = new Operations($databseConfig['server'], $databseConfig['database'], $databseConfig['username'], $databseConfig['password']);
