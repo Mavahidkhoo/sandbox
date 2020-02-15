e592735a6b8ba28b2cf4f4a980e6e8eb7d4ce3e3
androsein <androsein@yahoo.com>
2020-02-15 12:10:16 +0330

    Added uploadImage function to upload image from base64 string

--- server/class/utils.class.php
+++ server/class/utils.class.php
@@ -13,4 +13,15 @@ class Utils
         );
         exit();
     }
+
+    public static function uploadImage($base64Image)
+    {
+        $fileName = uniqid('', true) . ".png";
+        $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64Image));
+        if (file_put_contents('../../library/img/' . $fileName, $data)) {
+            return $fileName;
+        } else {
+            return false;
+        }
+    }
 }
