170048167952a251b4727e28dacc1e33bfbd2616
androsein <androsein@yahoo.com>
2020-02-15 21:37:46 +0330

    Added search ability for users list
    Added header and some comments

--- server/api/user.php
+++ server/api/user.php
@@ -2,6 +2,39 @@
 require_once 'master.php';
 switch ($_POST['CRUD']) {
     case 'list':
+        /*
+            @desc   This CRUD represent a list of users
+            @method POST
+            @params searchColumn [optional] (allowed values: multi-column [search in name, family, username and bio], name, username)
+                    searchStrin [optional] (at least 3 characters)
+        */
+        # Check search params are set or not
+        $search = ' 1 ';
+        if (isset($_POST['searchColumn']) && trim($_POST['searchColumn']) != '' && $_POST['searchString'] && trim($_POST['searchString'])) {
+            # Check searchString length
+            if (strlen($_POST['searchString']) < 3) {
+                Utils::result(true, array(
+                    'message' => 'Search string must be at least 3 characters'
+                ));
+            }
+            # Create where syntax for select
+            switch ($_POST['searchColumn']) {
+                case 'multi-column':
+                    $search = " CONCAT(name, ' ', family, ' ', username, ' ', bio) LIKE '%" . $_POST['searchString'] . "%' ";
+                    break;
+                case 'name':
+                    $search = " CONCAT(name, ' ', family) LIKE '%" . $_POST['searchString'] . "%' ";
+                    break;
+                case 'username':
+                    $search = " username LIKE '%" . $_POST['searchString'] . "%' ";
+                    break;
+                default:
+                    Utils::result(true, array(
+                        'message' => 'Invalid search parameter'
+                    ));
+                    break;
+            }
+        }
         $select = $operation->select(
             'users',
             array(
@@ -15,7 +48,7 @@ switch ($_POST['CRUD']) {
                 'telegram',
                 'github'
             ),
-            ' 1 '
+            $search
         );
         Utils::result(
             $select == 'rowCountFalse',
