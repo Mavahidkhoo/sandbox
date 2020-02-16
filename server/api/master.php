d59265195f109c05211cffd502323381657919e3
androsein <androsein@yahoo.com>
2020-02-16 22:52:19 +0330

    Added authenticate function

--- server/api/master.php
+++ server/api/master.php
@@ -11,6 +11,18 @@ if (!isset($_POST['CRUD'])) {
 }
 $operation = new Operations($databseConfig['server'], $databseConfig['database'], $databseConfig['username'], $databseConfig['password']);
 $currentDate = jdate('o', '', '', '', 'en') . jdate('m', '', '', '', 'en') . jdate('d', '', '', '', 'en');;
+
+/* 
+    @desc This function check if user signed in or not, if not return 'Unauthorized' and exit();
+*/
+function authenticate()
+{
+    if (!isset($_SESSION['signedIn']) || !isset($_SESSION['userId']) || !$_SESSION['signedIn']) {
+        Utils::result(true, array(
+            'message' => 'Unauthorized'
+        ));
+    }
+}
 function formatDate($date)
 {
     return substr($date, 0, 4) . '/' . substr($date, 4, 2) . '/' . substr($date, 6, 2);
