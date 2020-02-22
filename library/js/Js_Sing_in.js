014e4d2922c37982b761b36151464bf0a1904e77
M.a.vahidkhoo <m.a.vahidkhoo@gmail.com>
2020-02-22 16:38:33 +0330

    Create two js File for Sign-up and in

---
+++ library/js/Js_Sing_in.js
@@ -0,0 +1,4 @@
+//jquery Code For Sign_in page(account/index.html)
+$("Sing_in").ready(function() {
+    alert("Hello World !");
+});
---
+++ library/js/Js_Sing_up.js
@@ -0,0 +1,4 @@
+//jquery Code FOr Sign_up Page(account/index2.html)
+$("#Sign_up").ready(function() {
+    alert("Hello");
+});
