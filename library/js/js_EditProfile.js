c640b9dcb1d19cb6f1676e90d96240c136f65970
m.a.vahidkhoo <m.a.vahidkhoo@gmail.com>
2020-03-06 10:57:58 +0330

    work on edit pro but not comlete and not send to server

--- library/js/js_EditProfile.js
+++ library/js/js_EditProfile.js
@@ -1,30 +1,61 @@
 var editProfileurl = '../../server/api/account.php';
+var userInformationurl = '../../server/api/account.php';
+var loginurl = '../../server/api/account.php';
 $(document).ready(function() {
-
-
+    userInformation();
+    editProfile();
+    checkLogin();
+    var name;
+    var family;
+    var username;
+    var bio;
+    var linkedIn;
+    var instagram;
+    var telegram;
+    var github;
     $("#save").click(function() {
-        var name = $("#name").val();
-        var family = $("#family").val();
-        var username = $("#username").val();
-        var bio = $("#bio").val();
-        var linkedIn = $("#linkedIn").val();
-        var instagram = $("#instagram").val();
-        var telegram = $("#telegram").val();
-        var github = $("#github").val();
-        if (!(name == "" || family == "" || username == "")) {
-            editProfile();
 
-        }
-
+        name = $("#name").val();
+        family = $("#family").val();
+        username = $("#username").val();
+        bio = $("#bio").val();
+        linkedIn = $("#linkedIn").val();
+        instagram = $("#instagram").val();
+        telegram = $("#telegram").val();
+        github = $("#github").val();
     });
 
+    function userInformation() {
+        var data = {
+            CRUD: 'get'
+        };
+        var profileImage = btoa($("#profileImage").val());
+        var result = send(editProfileurl, data);
+        $("#imgPro").removeClass("d-none");
+        $("#userAcc").removeClass("d-none");
+        $("#userAcc").html(result.response.name);
+        $("#imgPro").attr("src", "../../library/img/" + result.response.profileImage);
+        var name2 = (result.response.name).split(' ');
+        name = name2[0];
+        family = name2[1];
+        $("#name").val(name2[0]);
+        $("#family").val(name2[1]);
+    };
 
     function editProfile() {
         var data = {
             CRUD: 'edit-profile'
         };
         var result = send(editProfileurl, data);
-        console.log(result);
-        console.log(result.response)
+    };
+
+    function checkLogin() {
+        var data = {
+            CRUD: 'check'
+        };
+        var result = send(loginurl, data);
+        $("#username").val(result.response.username);
+
+
     };
 })
