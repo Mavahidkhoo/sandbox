d11173ac289bd2532a12bd70331f84fef70582da
m.a.vahidkhoo <m.a.vahidkhoo@gmail.com>
2020-03-07 14:11:05 +0330

    Complete Update Post

--- library/js/Js_EditPost.js
+++ library/js/Js_EditPost.js
@@ -1,61 +1,58 @@
 url = '../../server/api/post.php'
-var body;
-var title;
 var postId;
 $(document).ready(function() {
 
     geturl();
+    get();
+    //console.log(postId);
+    $("#EditPost").click(function() {
+        update();
+
+        $("#modalsabt").append(`<div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">ویرایش پست</h4>
+                    </div>
+                    <div class="modal-body">
+                 <p id='p'>پست شما با موفقیت ویرایش شد.</p> 
+                 </div> 
+                 </div> 
+                 </div> 
+                 </div>`);
+        setTimeout(function() { window.location.href = "http://localhost/sandbox/Pages/home/index.html"; }, 1000);
+
+    })
+
+
+});
+
+function get() {
     var data = {
         CRUD: 'get',
         postId: postId,
     };
     var result = send(url, data);
     //console.log(result.response[0].body);
-    title = $("#title").val(result.response[0].title);
-    body = $("#PostText").val(result.response[0].body);
-    $("#EditPost").click(function() {
-        PostUpdate();
-    })
+    $("#title").val(result.response[0].title);
+    $("#PostText").val(result.response[0].body); {
 
+    }
+}
 
-    // if (result.response.message == "Post created successfully") {
-    //     $("#modalsabt").append(`<div class="modal fade" id="myModal" role="dialog">
-    //     <div class="modal-dialog">
-    //         <div class="modal-content">
-    //             <div class="modal-header">
-    //                 <h4 class="modal-title">ویرایش پست</h4>
-    //             </div>
-    //             <div class="modal-body">
-    //          <p id='p'>پست شما با موفقیت ویرایش شد.</p> 
-    //          </div> 
-    //          </div> 
-    //          </div> 
-    //          </div>`);
-    //     setTimeout(function() { window.location.href = "http://localhost/sandbox/Pages/home/index.html"; }, 1000);
-
-
-
-    // }
-
-
-
-
-});
-
-function PostUpdate() {
+function update() {
     var data = {
         CRUD: 'update',
         postId: postId,
-        title: title,
-        body: body
+        title: $('#title').val(),
+        body: $('#PostText').val()
     };
     var result = send(url, data);
-    console.log(result);
+    console.log(result.response);
 }
 
 function geturl() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     postId = urlParams.get('postid');
-
 }
