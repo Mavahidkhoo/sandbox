f37533b9a2f9048347467acccbbe7a51dcad64fe
m.a.vahidkhoo <m.a.vahidkhoo@gmail.com>
2020-03-16 13:48:41 +0330

    add link for userlist and delete elements if null

--- library/js/Js_userList.js
+++ library/js/Js_userList.js
@@ -74,15 +74,16 @@ function get() {
     };
     var result = send(listurl, data);
     //console.log(result.response.profileImage);
+    // <img class='float-left  rounded rounded-circle' alt="Deleted Image" src=../../library/img/${result.response.profileImage} width=100 height=100>
     $('#showUser2').append(`
         <div class=box3>
             نام : ${result.response.name ? result.response.name : null}<br>
-            بیوگرافی :${result.response.bio ? result.response.bio : ""}  <br>
-            <img class='float-left rounded rounded-circle' alt="Deleted Image" src=../../library/img/${result.response.profileImage} style=" height: 100px; width: 100px;">
-            لینکدن :${result.response.linkedIn ? result.response.linkedIn : ""}<br>
-            اینستاگرام : ${result.response.instagram ?result.response.instagram:"" }<br>
-            تلگرام :${result.response.telegram ? result.response.telegram:""}<br>
-            گیت هاب :${result.response.github ? result.response.github : ""}<br>
+          ${result.response.bio ? `بیوگرافی : ${result.response.bio}<br>`:''}
+      
+            ${result.response.linkedIn ? `لینکدن : <a class=text-info href=https://www.linkedin.com/in/${result.response.linkedIn}>${result.response.linkedIn}</a><br>` : ''}
+           ${result.response.instagram ?` اینستاگرام : <a class=text-info href=https://www.instagram.com/${result.response.instagram}>${result.response.instagram}</a><br>`:"" }
+          ${result.response.telegram ? `  تلگرام : <a class=text-info href=https://t.me/${result.response.telegram} >${result.response.telegram}</a><br>`:""}
+          ${result.response.github ? `  گیت هاب : <a class=text-info href=https://github.com/${result.response.github}>${result.response.github}</a><br> `: ""}
         </div>
 `);
 
